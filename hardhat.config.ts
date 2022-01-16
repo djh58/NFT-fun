62b7eb8a2950fa39c6e093f7453596f0f9832e8a
Dan Hepworth <danhep@gmail.com>
2022-01-16 15:01:46 -0500

    Add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
 import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
